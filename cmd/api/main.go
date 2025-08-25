9f2d01f1789271e14c8c66a1d5102948f950026e
Breno dos Santos Moreira <breno.dsmnb@gmail.com>
2025-08-24 21:59:05 -0300

    feat: adiciona integração do endpoint POST /campaigns no main

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/brenodsm/GoCampaign/internal/domain/campaign"
+	"github.com/brenodsm/GoCampaign/internal/endpoints"
+	"github.com/brenodsm/GoCampaign/internal/infrastructure/database"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	campaign_service := campaign.Service{
+		Repository: &database.CampaignRepository{},
+	}
+
+	handler := endpoints.Handler{
+		CampaignService: &campaign_service,
+	}
+
+	r.Post("/campaigns", handler.CampaignPost)
+
+	http.ListenAndServe(":5000", r)
 }
